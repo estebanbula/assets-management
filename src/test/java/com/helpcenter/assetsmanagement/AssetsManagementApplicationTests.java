cf7543c7d56356f8139d5b84a10b2ca3365f8346
Esteban Bula <carlosbula01@gmail.com>
2023-09-23 14:05:32 -0500

    Project structure

---
+++ src/test/java/com/helpcenter/assetsmanagement/AssetsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpcenter.assetsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
